d574615b222ca44fdb344ce2e752069cc659e913
diegoalzuet <diegoalzuet@gmail.com>
2021-11-26 23:49:20 -0300

    Desafio Angular Integrando conocimientos

---
+++ desafio-integrando-conocimientos/src/app/componente/componente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente',
+  templateUrl: './componente.component.html',
+  styleUrls: ['./componente.component.scss']
+})
+export class ComponenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
