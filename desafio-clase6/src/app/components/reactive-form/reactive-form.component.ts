df7ecdbffbe71fc96f969acfd0f6dd63b51e7698
diegoalzuet <diegoalzuet@gmail.com>
2021-12-04 02:03:46 -0300

    Desafio Clase 6

--- desafio-clase6/src/app/components/reactive-form/reactive-form.component.ts
+++ desafio-clase6/src/app/components/reactive-form/reactive-form.component.ts
@@ -1,3 +1,4 @@
+import { UsuarioRegistrado } from './../../models/usuariosRegistrados.model';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -8,9 +9,15 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ReactiveFormComponent implements OnInit {
 
+  usuario: UsuarioRegistrado = {
+    user: "Diego",
+    password: "1234567890"
+  }
+
+
   usuarioRegistradoForm = new FormGroup({
-    user: new FormControl('',[Validators.required]),
-    password: new FormControl('',[Validators.required,Validators.minLength(10)])
+    user: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(10)])
   });
 
   userControl = this.usuarioRegistradoForm.controls['user'];
@@ -21,8 +28,12 @@ export class ReactiveFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  loguear(){
-    console.log(this.usuarioRegistradoForm.value);
+  loguear() {
+
+    if (this.usuarioRegistradoForm.controls['user'].value == this.usuario.user && this.usuarioRegistradoForm.controls['password'].value == this.usuario.password)
+      console.log("Bienvenido. Login exitoso");
+    else
+      console.log('Error en las credenciales')
   }
 
 }
