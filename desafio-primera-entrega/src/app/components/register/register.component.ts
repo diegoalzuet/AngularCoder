4074d5f7d33d48781ac974f024338a89337f53e6
diegoalzuet <diegoalzuet@gmail.com>
2021-12-11 03:07:03 -0300

    se validaron los campos del componente sign up

--- desafio-primera-entrega/src/app/components/register/register.component.ts
+++ desafio-primera-entrega/src/app/components/register/register.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    password1: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    password2: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    check: new FormControl(false, [Validators.requiredTrue])
+  });
+
+  userControl = this.loginForm.controls['email'];
+  passwordControl1 = this.loginForm.controls['password1'];
+  passwordControl2 = this.loginForm.controls['password2'];
+  checkControl = this.loginForm.controls['check'];
 
   constructor() { }
 
